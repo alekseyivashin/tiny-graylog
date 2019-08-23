d0655135597f99908d38e52b3c4b944fd9ceecab
Ivashin, Aleksey <a.ivashin@itransition.com>
2019-08-23 16:53:51 +0300

    depersonalized test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -21,33 +21,12 @@ class ApplicationTest {
 
             handleRequest(HttpMethod.Post, "/") {
                 addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                setBody("""{"short_message": "ViewDoc", 
-                    |"requestDateTime": "22.08.2019 15:33:11", 
-                    |"browserUserAgent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36", 
-                    |"requestName": "ViewDoc", 
-                    |"pageBlock": "glossaryPage", 
-                    |"userName": "uliya8@yopmail.com", 
-                    |"UUID": "000f2438-69e8-4fa3-ad64-7db4b1ae92f1", 
-                    |"userCategory": "Бухгалтер (коммерческие) М", 
-                    |"isTrial": "false", 
-                    |"subscriptionID": "1201", 
-                    |"subscriptionName": "1201 Безлимит!", 
-                    |"accessCreditType": "null", 
-                    |"organizationID": "5", 
-                    |"organizationName": "UlyaOrganization, название, кторое не вмещается в одну строку в моем профиле для личных данных - поле компания", 
-                    |"infoBankName": "BELAW", 
-                    |"docId": 180409, 
-                    |"infoBankNameDocId": "BELAW_180409", 
-                    |"docKind": "Кодекс Республики Беларусь", 
-                    |"teamBlock": "AdminAdmin", 
-                    |"author": "Admin", 
-                    |"viewDocDurationId": null, 
-                    |"sortResults": "complex", 
-                    |"sectionFilter": "Все", 
-                    |"searchRequest": "термин Интернет-ссылка для отображения в блоке", 
-                    |"searchType": "quickSearch", 
-                    |"backspin": "false", 
-                    |"requestId": "9ca20a97-4a9b-4d45-8542-3dc9ec05fa8a"}""".trimMargin())
+                setBody(
+                    """{"short_message": "Doc", 
+                    |"requestDateTime": "12.12.1019 11:53:00", 
+                    |"browserUserAgent": "Agent", 
+                    |"requestName": "Doc"}""".trimMargin()
+                )
             }.apply {
                 assertEquals(HttpStatusCode.OK, response.status())
             }
